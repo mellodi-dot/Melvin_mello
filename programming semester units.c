6d963b9d10110f5b8e7e5ab56a8d581dc31444a6
mellodi-dot <mainamelvin18@gmail.com>
2025-09-21 22:28:44 +0300

    Add files via upload

---
+++ programming semester units.c
@@ -0,0 +1,19 @@
+/*
+Name:Melvin nderitu maina
+Reg:CT100/G/26132/25
+Description:semester units=
+*/
+
+#include <stdio.h>
+
+int main() {
+    printf("1. Analogue electronics\n");
+    printf("2. Mathematics for science\n");
+    printf("3. Computer systems and organisations\n");
+    printf("4. Fundamentals of ICT \n");
+    printf("5. Communication and information literacy skills\n");
+    printf("6. Essentials of accounting\n");
+    printf("7. Introduction to computer applications\n");
+    printf("8. Introduction to programming and algorithms\n");
+    return 0;
+}
