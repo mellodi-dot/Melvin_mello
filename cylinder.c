2ad1a19ee4c12f2fb57e2c7399cf2bc3611720f0
mellodi-dot <mainamelvin18@gmail.com>
2025-09-21 22:14:31 +0300

    Add files via upload

---
+++ cylinder.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#define PI 3.14159265359
+
+int main() {
+    double radius, height, volume, surface_area;
+
+    // Prompt user for input
+    printf("Enter the radius of the cylinder: ");
+    scanf("%lf", &radius);
+
+    printf("Enter the height of the cylinder: ");
+    scanf("%lf", &height);
+
+    // Calculate volume and surface area
+    volume = PI * radius * radius * height;
+    surface_area = 2 * PI * radius * radius + 2 * PI * radius * height;
+
+    // Display results
+    printf("Volume of the cylinder = %.2f\n", volume);
+    printf("Surface area of the cylinder = %.2f\n", surface_area);
+
+    return 0;
+}
